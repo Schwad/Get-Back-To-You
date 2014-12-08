eb31a6454e3b5e7d3615a8b52d781ffdbc03fda0
Schwad <nicholas.schwaderer@gmail.com>
2014-12-07 22:54:55 -0700

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ Get-Back-To-You
 ===============
 
 A quick, slick, phone call and email manager to save your life when you have a full inbox and voicemail.
+
+
+*Run corefile.rb*
+
+
+The standard way to do a command sets type, content and priority.Such as 'call jeff smith before thursday 2'.
+
+1 is the highest priority and down from there. 
+
+Other commands include 'ls all', 'ls call', 'ls email', and exit.
+
+You may also call 'taskno' to see the number of tasks.
+
+You may backup your data file, completed file, or deleted files with 'backup data' or 'backup completed' or 'backup removed'.
+
+ If you have an issue with this program you can report it at https://github.com/Schwad
---
+++ corefile.rb
@@ -0,0 +1,144 @@
+require 'csv'
+require 'fileutils'
+
+# Delete and remove still don't work, for some reason priority number is getting added in on the line content. adding and ID seems to be working fine
+#do what you did on line 36 to call and email
+
+puts "Time manager activate"
+puts ''
+puts ''
+file_path_data = "data.csv"
+file_path_data_2 = "data1.csv"
+file_path_completed = "complete.csv"
+file_path_removed = "removed.csv"
+destination_directory = "manager_data_backups"
+
+while true
+  puts ''
+  such_length = File.open("data.csv").readlines.size
+
+
+  command = gets.chomp
+  fname = "data.csv"
+  puts ''
+
+  if command.split(//)[0..2].join('') == "add"    
+
+     line_input = command.split(//)[4..-1].join('') + ",\n"
+     time = Time.new
+
+     if line_input.split(//)[0..3].join('') == "call"
+      line_command = line_input.split(//)[5..-5].join('')
+       CSV.open("data.csv", "ab") do |csv|
+        csv << [such_length,"call", line_command, "#{time.hour}:#{time.min} #{time.day}/#{time.month}", line_input.split(//)[-3] ]
+      end
+
+    elsif line_input.split(//)[0..4].join('') == "email"
+       line_command = line_input.split(//)[6..-5].join('')
+       CSV.open("data.csv", "ab") do |csv|
+        csv << [such_length,"email", line_command, "#{time.hour}:#{time.min} #{time.day}/#{time.month}", line_input.split(//)[-3] ]
+      end
+    end
+
+  elsif command.split(//)[0..5].join('') == "ls all"
+
+    CSV.open("data.csv", :row_sep => :auto, :col_sep => ",") do |csv|
+      csv.each {|a,b,c,d,e| puts "#{b} #{c}, added #{d}, priority level #{e}. ID number #{a}\n" unless a == "RowNum"}
+    end
+
+  elsif command.split(//)[0..7].join('') == "ls call"
+    
+    CSV.foreach("data.csv") do |row|
+      if row[1] == "call"
+        puts "#{row[1]} #{row[2]}, added #{row[3]}, priority level #{row[4]}. ID number #{row[0]}\n"
+      end
+    end
+
+  elsif command.split(//)[0..7].join('') == "ls email"
+    
+    CSV.foreach("data.csv") do |row|
+      if row[1] == "email"
+        puts "#{row[1]} #{row[2]}, added #{row[3]}, priority level #{row[4]}. ID number #{row[0]}\n"
+      end
+    end
+
+  elsif command.split(//)[0..3].join('') == "exit"
+    exit
+
+  elsif command.split(//)[0..5].join('') == "remove"
+    such_id = command.split(//)[7..-1].join('').to_i + 1
+    current_file = CSV.read("data.csv")
+    time = Time.new
+    timefile ="#{time.hour}:#{time.min}, #{time.day}/#{time.month}"
+
+    #complete the file
+    CSV.foreach("data.csv") do |csvbig|
+      if $INPUT_LINE_NUMBER == such_id
+          csvbig << timefile
+          CSV.open("removed.csv", "ab") do |csv|
+            csv << csvbig
+          end
+        puts "Task #{$INPUT_LINE_NUMBER - 1} removed!"
+      end
+    end
+
+    #delete the file
+    f = File::open("data.csv", "r")
+    dest=File::open("data1.csv", "w")
+    f.each_line do |line|
+      next if f.lineno == such_id
+      dest.write(line)
+    end
+    f.close
+    dest.close
+    FileUtils.cp(file_path_data_2, "data.csv")
+
+  elsif command.split(//)[0..7].join('') == "complete"
+    such_id = command.split(//)[9..-1].join('').to_i + 1
+    guests = CSV.read('data.csv',headers:true)
+    current_file = CSV.read("data.csv")
+    time = Time.new
+    timefile ="#{time.hour}:#{time.min}, #{time.day}/#{time.month}"
+
+    #complete the file
+    CSV.foreach("data.csv") do |csvbig|
+      if $INPUT_LINE_NUMBER == such_id
+          csvbig << timefile
+          CSV.open("complete.csv", "ab") do |csv|
+            csv << csvbig
+          end
+        puts "Task #{$INPUT_LINE_NUMBER - 1} completed!"
+      end
+    end
+
+    #delete the file
+    f = File::open("data.csv", "r")
+    dest=File::open("data1.csv", "w")
+    f.each_line do |line|
+      next if f.lineno == such_id
+      dest.write(line)
+    end
+    f.close
+    dest.close
+    FileUtils.cp(file_path_data_2, "data.csv")
+
+
+
+  elsif command.split(//)[0..5].join('') == "backup"
+    if command.split(//)[7..10].join('') == "data"
+      FileUtils.cp(file_path_data, destination_directory)
+      puts "Backed up data file."
+    elsif command.split(//)[7..10].join('') == "comp"
+      FileUtils.cp(file_path_completed, destination_directory)
+       puts "Backed up completed file."
+    elsif command.split(//)[7..10].join('') == "remo"
+      FileUtils.cp(file_path_removed, destination_directory)
+      puts "Backed up removed file."
+    else 
+      puts "backup failed, unclear instructions. Type 'help' for more info."
+    end
+
+  else
+    puts "The standard way to do a command sets type, content and priority.\n\n Such as 'call jeff smith before thursday 2'.\n\n 1 is the highest priority and down from there. Other commands include 'ls all', 'ls call', 'ls email', and exit. Those commands list your todos.\n\n You may also call 'taskno' to see the number of tasks.\n\n You may backup your data file, completed file, \n\n or deleted files with 'backup data' or \n\n 'backup completed' or 'backup removed'. \n\n If you have an issue with this program you can report it at https://github.com/Schwad"
+  end
+end
